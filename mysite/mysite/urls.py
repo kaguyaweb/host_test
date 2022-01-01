407e72ca589a3f583ab4cebfcb5005fe65ea7d94
kaguyaweb <h1810518@edu.cc.uec.ac.jp>
2022-01-01 21:32:43 +0900

    change url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,10 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('polls.urls')),
 ]
-#urlpatterns = [
-#    path('polls/', include('polls.urls')),
-#    path('admin/', admin.site.urls),
-#]
